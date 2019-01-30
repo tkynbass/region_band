d1603ca7bb19b2ba5784bf0d62ada464a85a81bc
erikka-22 <iwasaki.pingoo@gmail.com>
2019-01-31 08:11:16 +0900

    改行

--- iwasakierika.js
+++ iwasakierika.js
@@ -1,36 +1,11 @@
-// Description:
-//   Utility commands surrounding Hubot uptime.
-//
-// Commands:
-//   ping - Reply with pong
-//   echo <text> - Reply back with <text>
-//   time - Reply with current time
 'use strict';
-
-// module.exports = (robot) => {
-//   robot.respond(/PING$/i, (res) => {
-//     res.send('PONG');
-//   });
-
-//   robot.respond(/ADAPTER$/i, (res) => {
-//     res.send(robot.adapterName);
-//   });
-
-//   robot.respond(/ECHO (.*)$/i, (res) => {
-//     res.send(res.match[1]);
-//   });
-
-//   robot.respond(/TIME$/i, (res) => {
-//     res.send(`Server time is: ${new Date()}`);
-//   });
-
-  
-// };
-
 
 module.exports = (robot) =>{
   robot.respond(/日報$/i, (res) => {
     var number_of_menbers = 5;
+    var point_msg = [];
+    var rank_msg = [];
+    var task = ["コーヒーを飲む", "皿を洗う", "新聞を読む", "机を片付ける", "服をたたむ"]
     var point = [10,30,50,20,40];
     var point_sort = point.slice().sort(
       function(a,b){
@@ -38,13 +13,31 @@ module.exports = (robot) =>{
       }
     );
     var id = 0;
-    for (id = 0; id < number_of_menbers; id++){
-      res.send("ユーザ" + id + "の得点は" + point[id] + "です");
+    
+    var output_tasks = () => {
+      res.send(task.join("\n"));
     }
-    for (var id = 0; id < number_of_menbers; id++){
-      var rank = point_sort.indexOf(point[id]) + 1;
-      res.send("ユーザ" + id + "の順位は" + rank + "位です");
+
+    var output_points = () => {
+      for (id = 0; id < number_of_menbers; id++){
+        var point_msg_each = "ユーザ" + id + "の得点は" + point[id] + "です";
+        point_msg.push(point_msg_each);
+      }
+      res.send(point_msg.join("\n"));
     }
+    
+    var output_rank = () => {
+      for (var id = 0; id < number_of_menbers; id++){
+        var rank = point_sort.indexOf(point[id]) + 1;
+        var rank_msg_each = "ユーザ" + id + "の順位は" + rank + "位です";
+        rank_msg.push(rank_msg_each);
+      }
+      res.send(rank_msg.join("\n"));
+    }
+    
+    output_tasks();  
+    output_points();
+    output_rank();
   })
 };
   
